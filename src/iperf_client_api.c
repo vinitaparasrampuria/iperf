28a195efae106a22629d0f6650d1b7067ebdac5c
Vinita Parasrampuria <91571551+vinitaparasrampuria@users.noreply.github.com>
2023-08-21 16:35:58 -0400

    changed sleep value

--- src/iperf_client_api.c
+++ src/iperf_client_api.c
@@ -69,7 +69,9 @@ iperf_create_streams(struct iperf_test *test, int sender)
     int orig_bind_port = test->bind_port;
     for (i = 0; i < test->num_streams; ++i) {
 	int r = rand() % 11;
-	sleep(r*1000);
+	printf("%d", r);
+	sleep(r);
+	
         test->bind_port = orig_bind_port;
 	if (orig_bind_port) {
 	    test->bind_port += i;
